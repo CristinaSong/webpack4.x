bfbdb65bc6465c36906f166a5a4da24f31772200
scc <songchengcheng@baijiahulian.com>
2019-09-27 20:21:30 +0800

    评论内容1

--- src/components/CmtItem.jsx
+++ src/components/CmtItem.jsx
@@ -26,7 +26,7 @@ export default function CmtItem(props) {
     return <div className={cmtItem.item}>
     <h2>111</h2>
     <h1 className={cmtItem.user}>评论人：{props.user}</h1>
-    <p className={cmtItem.contend}>评论内容：{props.content}</p>
+    <p className={cmtItem.contend}>评论内容1：{props.content}</p>
 </div>
 }
 
